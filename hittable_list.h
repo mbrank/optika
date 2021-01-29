dc144ea9eaa9b819bb4754c3b2b3e47f609d29f3
Matic Brank <matic.brank@lecad.fs.uni-lj.si>
2021-01-29 16:35:45 +0100

    Added multiple spheres to python version.

--- hittable_list.h
+++ hittable_list.h
@@ -10,20 +10,25 @@ using std::shared_ptr;
 using std::make_shared;
 
 class hittable_list : public hittable {
-    public:
-        hittable_list() {}
-        hittable_list(shared_ptr<hittable> object) { add(object); }
+public:
+  hittable_list() {}
+  hittable_list(shared_ptr<hittable> object) { add(object); }
+  
+  void clear() { objects.clear(); }
+  void add(shared_ptr<hittable> object) { objects.push_back(object); }
+  
 
-        void clear() { objects.clear(); }
-        void add(shared_ptr<hittable> object) { objects.push_back(object); }
+  // class hittable_list will have method hit that will override
+  // virtual method from hittable class
+  virtual bool hit(
+				   const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
-        virtual bool hit(
-            const ray& r, double t_min, double t_max, hit_record& rec) const override;
-
-    public:
-        std::vector<shared_ptr<hittable>> objects;
+public:
+  std::vector<shared_ptr<hittable>> objects;
 };
 
+// method hit of class hittable_list, that overrides virtual method
+// hit from base class hittable
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     hit_record temp_rec;
     bool hit_anything = false;
